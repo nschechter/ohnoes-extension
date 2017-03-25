92ee5ff7e2dee332309b68136adc99ab3d7d38fe
Noah Schechter <noahschechter7@gmail.com>
2017-03-25 13:32:18 -0400

    added beaver

--- hoppy.js
+++ hoppy.js
@@ -6,12 +6,12 @@ const TICK_RATE = 500; // 1s
 
 const getRandomElement = () => {
 	arr = Array.from(document.body.getElementsByTagName("*"))
-	.filter((e) => { return typeof e.className === 'string' && !e.className.includes("ohnoes") });
+	.filter((e) => { return typeof e.className === 'string' && !e.className.includes("ohnoes") && !e.className.includes("hopper") });
 
 	return arr.length > 0 ? arr[Math.floor(Math.random()*arr.length)] : document.body 
 }
 
-const spawnBeaver = () => {
+const spawnErrorBuddy = () => {
 	// create image element that fades in
 	let img = document.createElement('img');
 	img.src = chrome.extension.getURL("images/OhNoes.png");
@@ -28,8 +28,42 @@ const spawnBeaver = () => {
 	ele.appendChild(text);
 };
 
+const spawnBeaver = () => {
+	// create image element that fades in
+	let img = document.createElement('img');
+	img.src = chrome.extension.getURL("images/Hopper-Jumping.png");
+	img.style.zIndex = "1";
+	img.className = "hopper-jumping";
+
+	// create text element that pops up
+	let text = document.createElement('div')
+	text.style.zIndex = "1";	
+	text.innerHTML = "You did it!";
+	text.className = "hopper-jumping-text";
+	ele = getRandomElement();
+	ele.appendChild(img);
+	ele.appendChild(text);
+};
+
+const spawnSuccessBeaver = () => {
+	// create image element that fades in
+	let img = document.createElement('img');
+	img.src = chrome.extension.getURL("images/Hopper-Cool.png");
+	img.style.zIndex = "1";
+	img.className = "hopper-jumping";
+
+	// create text element that pops up
+	let text = document.createElement('div')
+	text.style.zIndex = "1";	
+	text.innerHTML = "Congratulations! You earned " + Math.floor(Math.random() * 2500) + 200 " points!";
+	text.className = "hopper-jumping-text"
+	ele = getRandomElement();
+	ele.appendChild(img);
+	ele.appendChild(text);
+};
+
 tick = setInterval(() => {
-	spawnBeaver();
+	(Math.floor(Math.random() * 2) + 1) == 1 ? spawnErrorBuddy() : spawnBeaver()
 }, TICK_RATE);
 
 })();
